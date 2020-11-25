8338e3d173ff05bbeea1a3bf9ed2bb48376b90f1
a7elwafi <anouar0elwafi@gmail.com>
2020-11-25 16:09:49 +0100

    J unit tests

---
+++ test/voitures/AutoTest.java
@@ -0,0 +1,88 @@
+package voitures;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+
+import voitures.Auto;
+import static org.hamcrest.CoreMatchers.is;
+
+public class AutoTest {
+
+	
+
+	@Rule
+	  public ErrorCollector collector= new ErrorCollector();
+	
+	public AutoTest() {
+	}
+
+	@Before
+	public void setUp() {
+		// TODO
+	}
+
+	@After
+	public void tearDown() {
+	}
+	
+	/**
+	 * Test of getMarque method, of class Auto.
+	 */
+	@Test
+	public void testGetMarque() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of getModele method, of class Auto.
+	 */
+	@Test
+	public void testGetModele() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test of getPrix method, of class Auto.
+	 */
+	@Test
+	public void testGetPrix() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test of getConso method, of class Auto.
+	 */
+	@Test
+	public void testGetConso() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of getKilometrage method, of class Auto.
+	 */
+	@Test
+	public void testGetKilometrage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test of getNbPortes method, of class Auto.
+	 */
+	@Test
+	public void testGetNbPortes() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of getNbPlaces method, of class Auto.
+	 */
+	@Test
+	public void testGetNbPlaces() {
+		fail("Not yet implemented"); // TODO
+	}
+}
---
+++ test/voitures/GarageTest.java
@@ -0,0 +1,112 @@
+package voitures;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+
+import voitures.Garage;
+import static org.hamcrest.CoreMatchers.is;
+
+public class GarageTest {
+
+	
+	@Rule
+	  public ErrorCollector collector= new ErrorCollector();
+	
+	public GarageTest() {
+	}
+
+	@Before
+	public void setUp() {
+		// TODO
+	}
+
+	@After
+	public void tearDown() {
+	}
+	
+	/**
+	 * Test of f1 method, of class Garage.
+	 */
+	@Test
+	public void testF1() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f2 method, of class Garage.
+	 */
+	@Test
+	public void testF2() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f3 method, of class Garage.
+	 */
+	@Test
+	public void testF3() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f4 method, of class Garage.
+	 */
+	@Test
+	public void testF4() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f5 method, of class Garage.
+	 */
+	@Test
+	public void testF5() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f6 method, of class Garage.
+	 */
+	@Test
+	public void testF6() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f7 method, of class Garage.
+	 */
+	@Test
+	public void testF7() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f8 method, of class Garage.
+	 */
+	@Test
+	public void testF8() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f9 method, of class Garage.
+	 */
+	@Test
+	public void testF9() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test of f10 method, of class Garage.
+	 */
+	@Test
+	public void testF10() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
